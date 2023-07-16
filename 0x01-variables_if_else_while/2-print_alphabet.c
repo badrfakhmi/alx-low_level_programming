732d9681923c305bd66a36a0502f8d8b9db9108d
badrfakhmi <badrfakhmi@gmail.com>
2023-07-16 15:53:55 -0700

    w

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,14 @@
 
 int main(void)
 {
-
 	char c;
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar('\n');
-Return(0);
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (o);
 }
